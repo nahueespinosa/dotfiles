ba3c2e1febf7568dd5d169dc2bcbd6c43dee153a
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-06-17 23:15:56 -0300

    Fix oh-my-zsh install

--- pre-install.sh
+++ pre-install.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo 'Installing oh-my-zsh'
-  curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-fi
-
-if [ ! $SHELL = "/bin/zsh" ]; then
-  echo 'Changing default shell to zsh, you will have to log out and in again to see the changes'
-  chsh -s /bin/zsh
+  export CHSH=yes       # Make zsh the default shell
+  export RUNZSH=no      # Don't run zsh after the install
+  export KEEP_ZSHRC=yes # Don't replace an existing .zshrc
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
