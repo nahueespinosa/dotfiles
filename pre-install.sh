9d088c9ef1e3c787f62459ec40a7280a2843663b
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-07-03 13:57:50 -0300

    Add oh-my-zsh as a submodule (#16)
    * Add oh-my-zsh as submodule
    
    * Ignore dirty submodules
    
    * Add chsh command
    
    * Update README.md

--- pre-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  export CHSH=yes       # Make zsh the default shell
-  export RUNZSH=no      # Don't run zsh after the install
-  export KEEP_ZSHRC=yes # Don't replace an existing .zshrc
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
